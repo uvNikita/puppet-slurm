8dd550977d6aae26904f08c4b85503ca4f104515
Nikita Uvarov <uv.nikita@gmail.com>
2021-06-07 15:15:29 +0200

    Revert "Allow to override logdir"
    This reverts commit 9ff24ad9767b8e7ee219142f3b2064502c1d84f8.

--- manifests/config.pp
+++ manifests/config.pp
@@ -27,7 +27,7 @@ class slurm::config {
     $slurm::configdir,
     #$slurm::params::piddir,  # NO support for specialized piddir as per service definitions
     # $pluginsdir,
-  ] + ($slurm::logdir ? { undef => [], default => [ $slurm::logdir ] })
+  ] + ($slurm::params::logdir ? { undef => [], default => [ $slurm::params::logdir ] })
  if $slurm::ensure == 'present' {
     file { $slurmdirs:
       ensure => 'directory',
